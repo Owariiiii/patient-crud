b317a6ec14341e732045cf0b1fbd48a0861cb97f
Owariiiii <owarininsei@gmail.com>
2026-09-18 14:24:19 +0800

    Add environment configuration

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  production: true,
+  firebaseConfig: {
+    apiKey: 'AIzaSyB67jNy0JuHvxoTLhliO9LnRjwQU1Enouc',
+    authDomain: 'user-crud-5f7e0.firebaseapp.com',
+    databaseURL: 'https://user-crud-5f7e0-default-rtdb.asia-southeast1.firebasedatabase.app',
+    projectId: 'user-crud-5f7e0',
+    storageBucket: 'user-crud-5f7e0.firebasestorage.app',
+    messagingSenderId: '352301846296',
+    appId: '1:352301846296:web:869264ef4ecd837d6c186d',
+    measurementId: 'G-NRJ62JQ25X',
+  },
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,26 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebaseConfig: {
+    apiKey: 'AIzaSyB67jNy0JuHvxoTLhliO9LnRjwQU1Enouc',
+    authDomain: 'user-crud-5f7e0.firebaseapp.com',
+    databaseURL: 'https://user-crud-5f7e0-default-rtdb.asia-southeast1.firebasedatabase.app',
+    projectId: 'user-crud-5f7e0',
+    storageBucket: 'user-crud-5f7e0.firebasestorage.app',
+    messagingSenderId: '352301846296',
+    appId: '1:352301846296:web:869264ef4ecd837d6c186d',
+    measurementId: 'G-NRJ62JQ25X',
+  },
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
